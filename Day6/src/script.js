d07f6b62802f4f9475316b5d834766bd9e317c55
thebreezyanupam <theanupamofficial@gmail.com>
2025-03-13 17:37:31 -0400

    particle distortion

---
+++ Day6/src/script.js
@@ -0,0 +1,130 @@
+import * as THREE from 'three'
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const textureImg = textureLoader.load('/2.jpg')
+
+/**
+ * Test mesh
+ */
+// Geometry
+const geometry = new THREE.PlaneGeometry(480 * 1.5, 800 * 1.5, 480, 800)
+
+// Material
+const material = new THREE.ShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+    uniforms: {
+        uTexture: { value: textureImg },
+        uTime: { value: 0 },
+        UResolution: { value: new THREE.Vector4() },
+        uvRate1: { value: new THREE.Vector2(1, 1) },
+        uProgress: { value: 0.0 },
+        uDistortion: { value: 0 } // Add the uDistortion uniform
+    }
+})
+
+// Mesh
+const mesh = new THREE.Points(geometry, material)
+scene.add(mesh)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 5000)
+camera.position.set(0.25, -0.25, 1000)
+scene.add(camera)
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+
+
+/**
+ * Distortion using lil-gui
+ */
+const distortionControls = { uDistortion: 0.0 }
+gui.add(distortionControls, 'uDistortion', 0, 1, 0.01).onChange((value) => {
+    // Update the uDistortion uniform based on the slider value
+    material.uniforms.uDistortion.value = value
+})
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update the time uniform in the shader
+    material.uniforms.uTime.value = elapsedTime
+    
+
+    // Update controls
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
---
+++ Day6/src/shaders/fragment.glsl
@@ -0,0 +1,12 @@
+ varying vec2 vUv;
+ uniform sampler2D uTexture;
+ uniform float uBloomStrength;
+
+    void main() {
+        vec4 color = texture2D(uTexture, vUv);
+        // gl_FragColor = vec4(vUv,0.,1.);
+        gl_FragColor = color; 
+        if(color.r<0. && color.g <0.1 && color.b<0.1)
+        discard;// Simple gradient
+        
+    }
